4ef7dc93340ef07940a51298789dbcdea3e753c1
Sandra Õnne <sandraonne@gmail.com>
2019-03-30 13:31:27 +0200

    Created home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  resources :pages
+  resources :groups
+  resources :news
+  resources :menus
+  resources :workers
+  
   get 'home/index'
 
-  root 'home#index'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
