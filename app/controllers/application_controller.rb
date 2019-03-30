c57162c74997f255c2406bd058f7c9d40b7a15eb
Sandra Õnne <sandraonne@gmail.com>
2019-03-30 14:56:27 +0200

    Create Admin view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
-        workers_path
+        admin_index_path
     end
     
     def after_sign_out_path_for(scope)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'admin/index'
   resources :groups
   resources :pages
   resources :news
   resources :menus
   resources :workers
-  
+
   get 'home/index'
 
   devise_scope :user do
@@ -12,5 +13,9 @@ Rails.application.routes.draw do
   end
   
   devise_for :users
+
+  scope :admin do
+    root to: "admin#index"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
