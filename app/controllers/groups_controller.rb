6beb513d5d6f24a6799b6347990feaca15246639
Sandra Õnne <sandraonne@gmail.com>
2019-03-31 13:35:09 +0300

    Added image upload

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -70,6 +70,6 @@ class GroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params.require(:group).permit(:name, :number, :age, :description, :teacher, :phone)
+      params.require(:group).permit(:name, :number, :age, :description, :teacher, :phone, :avatar)
     end
 end
