507c00a8147a25ee5b55df344c36986c297ba3ca
Martin Kangur <martin@cariba.ee>
2019-05-21 11:40:38 +0300

    navigation options

--- app/models/navigation.rb
+++ app/models/navigation.rb
@@ -1,5 +1,16 @@
 class Navigation < ApplicationRecord
-  def available_pahts
-    
+  def available_paths
+    static_paths + available_pages
+  end
+  
+  def static_paths
+    [
+      {name: 'example', path: 'example'},
+      {name: 'test', path: 'test'}
+    ]
+  end
+
+  def available_pages
+    Page.all.map{|p| {name: p.heading, path: p.slug}}
   end
 end
