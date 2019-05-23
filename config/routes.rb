1d75c859675abfbbdcab31c6effe743de556816e
Sandra Õnne <sandraonne@gmail.com>
2019-05-23 15:36:25 +0300

    Login fixes

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
 
   #resources :pages, only: ['show']
   
-  devise_for :users
+  devise_for :users, :controllers => { :sessions => "sessions"}
 
   root to: 'web#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
